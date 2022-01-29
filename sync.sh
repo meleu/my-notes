96abab64b648f682e3d455443633880e00a8b931
meleu <meleu.dev@gmail.com>
2022-01-29 12:07:15 -0300

    Automated sync: ?? sync.sh

---
+++ sync.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# BUG: se por um acaso o script não concluir o push com sucesso, a próxima
+# execução não fará o push (git status não vai pegar mudança alguma).
+
+main() {
+  gitStatus="$(git status --porcelain)"
+
+  [[ -z "${gitStatus}" ]] && return 0
+
+  git pull --rebase \
+  && git add --all \
+  && git commit -m "Automated sync: ${gitStatus}" \
+  && git push
+}
+
+main "$@"
